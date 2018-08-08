ab8c9ab5674f8e4a329657a4376157fd769931f7
Snowbird <jakounter@yahoo.com>
2018-08-07 21:12:31 -0500

    functional

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,23 +9,35 @@ module.exports = function(app) {
       });
     app.post("/data/friends", function(req, res) {
         var newFriendScoresArray=req.body.scores
-        console.log(newFriendScoresArray)
-          // app.get("/data/friends", function(req, res) {
-          //   var friendComparisons= []
-          //   for(i=0;i<res.length; i++){
-          //     res[i].scores.forEach((e1)=>newFriendScoresArray.forEach((e2)=>{
-          //       comparisons=[]
-          //       comparison=Math.abs(e1-e2)
-          //       comparisons.push(comparison)
-          //       console.log(comparisons)
-          //       friendComparisons.push(sum(comparisons))
-          //     }));
-          //   }
-          //   bestMatch = Math.min(friendComparisons) 
-          // });
+        // console.log(newFriendScoresArray)
+        // app.get("/data/friends", function(req, res) {
+        var friendComparisons= []
+        for(i=0;i<friends.length; i++){
+          comparisons=[]
+          for(j=0;j<friends[i].scores.length; j++){
+            // console.log(friends[i].scores[j])
+            
+            // console.log(newFriendScoresArray[j])
+            comparison=Math.abs(friends[i].scores[j]-newFriendScoresArray[j])
+            comparisons.push(comparison) 
+          }
+          // console.log(comparisons)
+            summedComparisons =comparisons.reduce(function(total,number){
+              return total + number
+            })
+            // console.log(summedComparisons)
+            friendComparisons.push(summedComparisons);
+        }
+        // console.log(friendComparisons)
+        // console.log(Math.min.apply(null,friendComparisons))
+        bestMatchValue=Math.min.apply(null,friendComparisons)
+        bestMatchIndex=friendComparisons.indexOf(bestMatchValue)
+        // console.log(bestMatchIndex)
+        bestMatch = friends[bestMatchIndex]
         var newFriend = req.body;
         console.log(newFriend);
         friends.push(newFriend);
-        res.json(newFriend);
+        res.json(bestMatch);
       });
+
 }
